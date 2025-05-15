196a260cb7fc116f94a242941d3d2145e25e6a95
RiTa-23 <rita.tifrt@gmail.com>
2025-05-16 02:32:51 +0900

    wip

--- Assets/Scripts/BikeController.cs
+++ Assets/Scripts/BikeController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BikeController : MonoBehaviour
 {
-    float speed = 10f;
+    public float speed =5f;
     public float tiltValue = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,26 +13,32 @@ public class BikeController : MonoBehaviour
     public void ChangeTiltValue(float value)
     {
         tiltValue = value;
-    }   
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            tiltValue = -1f;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            tiltValue = 1f;
-        }
-
         //ずっと前進
         transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
-        //傾きの値によって左右に移動
-        transform.position += new Vector3(tiltValue*5, 0, 0) * Time.deltaTime;
+        if (transform.position.x < 5 && transform.position.x > -5)
+        {
+            //傾きの値によって左右に移動
+            transform.position += new Vector3(tiltValue * 5, 0, 0) * Time.deltaTime;
 
-        //傾きの値によって傾ける
-        transform.rotation = Quaternion.Euler(0, 0, -tiltValue * 30f);
+            //傾きの値によって傾ける
+            transform.rotation = Quaternion.Euler(0, 0, -tiltValue * 30f);
+        }
+        else
+        {
+            //画面外に出たら反対側から出てくる
+            if (transform.position.x > 5)
+            {
+                transform.position += new Vector3(-0.01f, 0, 0);
+            }
+            else if (transform.position.x < -5)
+            {
+                transform.position += new Vector3(0.01f, 0, 0);
+            }
+        }
     }
 }
