9990787bc2e245785993e18819dce213e857050f
RiTa-23 <rita.tifrt@gmail.com>
2025-05-19 16:58:27 +0900

    add:(run)ゲームオーバー条件ゆるめ

--- Assets/Scripts/Run/run_obstacles.cs
+++ Assets/Scripts/Run/run_obstacles.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
 
 public class run_obstacles : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,12 +23,18 @@ public class run_obstacles : MonoBehaviour
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             if(!playerController.isGameOver)
             {
-                float speed = playerController.speed;
-                playerController.GameOver();
+                float speed =playerController.speed ;
 
-                //x軸負の方向に力を加える
-                Vector3 direction = new Vector3(-1, 1, 0);
-                collision.gameObject.GetComponent<Rigidbody>().AddForce(direction * 3, ForceMode.Impulse);
+                if (speed > 2f)
+                {
+                    //スピードを下げる
+                    playerController.speed = 1f;
+                    //x軸負の方向に力を加える
+                    Vector3 direction = new Vector3(-1, 1, 0);
+                    collision.gameObject.GetComponent<Rigidbody>().AddForce(direction * 3, ForceMode.Impulse);
+                    rb.AddForce(-direction  * speed, ForceMode.Impulse);
+
+                }
             }
             
         }
